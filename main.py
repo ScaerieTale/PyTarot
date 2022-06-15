f335e2ef0f717f81785dd28a161ce6e5392b6f9b
ScaerieTale <7010800+ScaerieTale@users.noreply.github.com>
2022-06-15 15:29:42 -0500

    Add functional CLI front end that allows user to select number of cards they wish to draw.
    Includes failsafe exceptions for too high, low, or non number/nonsense entries.

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import spread
-
-spread.spread_of_(22)
-input()
+divining = True
+while divining is True:
+    spread_choice = int(input("Choose how many cards to draw: "))
+    if spread_choice > 22:
+        print("There are only 22 cards total within the Major Arcana deck.  Each card may only be drawn once per session.")
+    elif spread_choice < 0:
+        print("Please choose a positive number...")
+    elif spread_choice > 0 and spread_choice <= 22:
+        spread.spread_of_(spread_choice)
+        
+    else:
+        print("Do not trifle lightly where angels fear to tread.")
+    
+    reshuffle = input("Reshuffle the deck and try again? (y/n): ").lower
+    if reshuffle == 'n' or reshuffle == 'no':
+        input("Be sure to write down or copy and paste your reading. :)")
+        divining = False
